484f3bb65228d49ef27fd1159c284dbaa86d14e3
mutasem <mutasemms198@gmail.com>
2025-11-09 22:16:38 +0300

    first commit

--- database/migrations/2025_09_20_164516_create_notifications_table.php
+++ database/migrations/2025_09_20_164516_create_notifications_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('title');
             $table->string('body');
             $table->timestamps();
