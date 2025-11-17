c0e55addd3f0a077d09571fe0e6867e43788eed1
mutasem <mutasemms198@gmail.com>
2025-11-17 21:57:58 +0300

    first commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
 
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'date_of_birth' => 'date',
     ];
 
     public function families()
