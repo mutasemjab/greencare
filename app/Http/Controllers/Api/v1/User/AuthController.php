d413b06bee1fec1404244957a219ab587759a883
mutasem <mutasemms198@gmail.com>
2026-05-22 15:00:42 +0300

    first commit

--- app/Http/Controllers/Api/v1/User/AuthController.php
+++ app/Http/Controllers/Api/v1/User/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
 
     public function flagForAppstore()
     {
-        $flag = 1;
+        $flag = 2;
         return response()->json([
             'status' => true,
             'message' => $flag == 1 ? 'الميزات مرئية' : 'الميزات مخفية',
